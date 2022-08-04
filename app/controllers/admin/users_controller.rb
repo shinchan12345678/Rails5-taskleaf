045d5cd3c82dcf0ab326be574f86a207871c7feb
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-08-04 11:33:26 +0900

    practice

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,8 +3,8 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.new(user_path)
+  def create
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました"
@@ -13,10 +13,32 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def show
+    @user = User.find(params[:id]) 
   end
 
   def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました" 
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました"
   end
 
   private
